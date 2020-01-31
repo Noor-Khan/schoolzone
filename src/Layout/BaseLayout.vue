540ad517bfd2d8df7a688837fad163a84c1b0e23
noor-khan <foruconnect@gmail.com>
2020-01-31 12:04:38 +0500

    first commit

---
+++ src/Layout/BaseLayout.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <v-content>
+      <div class="base-layout">
+        <router-view />
+      </div>
+    </v-content>
+  </div>
+</template>
+<script>
+export default {
+  components: {}
+};
+</script>
+<style lang="scss" scoped>
+.base-layout {
+  position: relative;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="Home">
+    <h2>Hello World!</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {}
+};
+</script>
